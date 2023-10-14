6e66d8e2054a88372dafd787c54c17c8eb77f7e9
Carlos Pineda <cara_p@unicah.edu>
2023-10-14 16:18:50 -0600

    Escuchando a eventos y agregando event handlers

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,9 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = props => {
+  const clickHandler = () => {
+    console.log('Clicked!!!');
+  }
 
   return (
     <Card className={"expense-item"}>
@@ -14,6 +17,7 @@ const ExpenseItem = props => {
         <h2>{props.title}</h2>
         <div className={"expense-item__price"}>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   )
 }
