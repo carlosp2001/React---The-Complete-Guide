c77ccc9d5963082cf24e5c395382b1f32dfd7813
Carlos Pineda <cara_p@unicah.edu>
2023-10-14 19:44:05 -0600

    Trabajando con state

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,22 @@
+import {useState} from "react";
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 import './ExpenseItem.css';
 
 const ExpenseItem = props => {
-  const clickHandler = () => {
-    console.log('Clicked!!!');
+  const [title, setTitle] = useState(props.title);
+
+  // let title = props.title;
+  //
+  // const clickHandler = () => {
+  //   title = 'Updated!'
+  // }
+
+  const clickHandler = async () => {
+    await setTitle('Updated!');
+    console.log(title);
   }
 
   return (
@@ -14,7 +25,7 @@ const ExpenseItem = props => {
       {/*conocer en que lugar debemos ubicar el archivo*/}
       <ExpenseDate date={props.date}/>
       <div className={"expense-item__description"}>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className={"expense-item__price"}>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change title</button>
