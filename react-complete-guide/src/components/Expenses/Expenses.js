a24a0693e39096e4ba4412d776b6b55d19932ddb
Carlos Pineda <cara_p@unicah.edu>
2023-10-24 16:45:07 -0600

    Renderizando listas de datos con JSX

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -18,27 +18,18 @@ const Expenses = ({ items: expenses }) => {
   return (
     <div>
       <Card className={"expenses"}>
-        <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
+        <ExpensesFilter
+          onChangeFilter={filterChangeHandler}
+          selected={filteredYear}
         />
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
-        <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        />
+        {expenses.map((expense, index) => (
+          <ExpenseItem
+            key={index}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
