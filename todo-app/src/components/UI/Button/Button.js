7d86b1db954fe58d369ace89f54d67a42db22fbd
Carlos Pineda <cara_p@unicah.edu>
2023-10-28 22:42:36 -0600

    Usando CSS modulos

--- todo-app/src/components/UI/Button/Button.js
+++ todo-app/src/components/UI/Button/Button.js
@@ -1,41 +1,50 @@
 import React from 'react';
 
-import './Button.css';
-import styled from "styled-components";
+import styles from './Button.module.css';
 
-const ButtonStyled = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
-
-  @media (min-width: 768px) {
-    width: auto;
-  }
-
-  .button:focus {
-  outline: none;
-  }
-
-.button:hover,
-.button:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}
-
-`
+//import styled from "styled-components";
+//
+//const ButtonStyled = styled.button`
+//  width: 100%;
+//  font: inherit;
+//  padding: 0.5rem 1.5rem;
+//  border: 1px solid #8b005d;
+//  color: white;
+//  background: #8b005d;
+//  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//  cursor: pointer;
+//
+//  @media (min-width: 768px) {
+//    width: auto;
+//  }
+//
+//  .button:focus {
+//  outline: none;
+//  }
+//
+//.button:hover,
+//.button:active {
+//  background: #ac0e77;
+//  border-color: #ac0e77;
+//  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//}
+//
+//`
+//
+//const Button = props => {
+//  return (
+//    <ButtonStyled type={props.type} onClick={props.onClick}>
+//      {props.children}
+//    </ButtonStyled>
+//  );
+//};
+//
 
 const Button = props => {
   return (
-    <ButtonStyled type={props.type} onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
-    </ButtonStyled>
+    </button>
   );
 };
-
 export default Button;
